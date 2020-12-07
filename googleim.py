d1641078a3e4d428e6cd4ba44d91daf07c327089
sprumin <sprumin@gmail.com>
2020-12-07 23:20:33 +0900

    set default class

---
+++ googleim.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+
+import numpy as np
+import cv2
+import uuid
+import os
+import re
+import requests
+import sys
+import time
+
+
+
+class Googleim(object):
+    def __init__(self):
+        self.driver = webdriver.Chrome("chromedriver")
+
+    def crawl_image(self):
+        pass
+
+    def save_image(self):
+        pass
+
+    def download_log(self):
+        pass
+
+    def is_gif(self):
+        pass
